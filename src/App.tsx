4ec9693865979ec28b4b79dbc87807346b180da9
Bartłomiej Widlarz <bartlomiejwidlarz@gmail.com>
2018-02-18 13:29:55 +0100

    Introducing typescript with linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -14,9 +8,9 @@ import {
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Shake or press menu button for dev menu',
 });
 
 type Props = {};
